7adac83904f425364a124e81e9ba15a6b6e192ca
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-12-23 10:36:37 +0900

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,12 +1,7 @@
-; boot.asm
-bits 16             ; 16-bit mode
-
-org 0x7c00          ; Address where BIOS loads the boot sector
-
+bits 16
+org 0x7c00
 jmp short start
 nop
-
-; BIOS Parameter Block (BPB) - minimal
 OEMLabel:           db "MYOS    "
 BytesPerSector:     dw 512
 SectorsPerCluster:  db 1
@@ -26,75 +21,54 @@ BootSignature:      db 0x29
 VolumeID:           dd 0
 VolumeLabel:        db "NO NAME    ", 0
 FSType:             db "FAT12   "
-
 start:
-    ; Set up data segments
     mov ax, cs
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7c00      ; Set up stack pointer
-
-    ; Clear the screen
+    mov sp, 0x7c00
     mov ax, 0xb800
     mov gs, ax
     mov di, 0
 clear_screen:
-    mov word [gs:di], 0x0F00  ; Space with white on black
+    mov word [gs:di], 0x0F00
     add di, 2
-    cmp di, 4000            ; For 80x25 mode
+    cmp di, 4000
     jne clear_screen
-
-    ; Load the kernel
-    mov ah, 0x02          ; BIOS read function
-    mov al, 1             ; Read 1 sector
-    mov ch, 0             ; Cylinder 0
-    mov cl, 2             ; Sector 2 (after boot sector)
-    mov dh, 0             ; Head 0
-    mov dl, 0x00          ; Drive 0 (floppy)
-    mov bx, 0x10000       ; Load address
-    int 0x13              ; Call BIOS
-
-    ; Enter protected mode (simplified)
-    cli                     ; Disable interrupts
-    lgdt [gdt_ptr]          ; Load GDT
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x00
+    mov bx, 0x10000
+    int 0x13
+    cli
+    lgdt [gdt_ptr]
     mov eax, cr0
-    or eax, 0x1             ; Set PE bit
+    or eax, 0x1
     mov cr0, eax
-    jmp 08h:kernel_start    ; Long jump
-
-bits 32             ; 32-bit mode
+    jmp 08h:kernel_start
+bits 32
 kernel_start:
-    ; Re-initialize data segments
-    mov ax, 0x10        ; Data segment in GDT
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov esp, 0x90000    ; Set up stack pointer
-
-    ; Jump to C kernel
+    mov esp, 0x90000
     extern kmain
     call kmain
-
-    ; Infinite loop
     cli
 halt_loop:
     hlt
     jmp halt_loop
-
-; Global Descriptor Table (GDT)
 gdt_start:
-    dd 0x0                ; Null descriptor
-    ; Code segment (privilege level 0)
+    dd 0x0
     dw 0xFFFF, 0x0000, 0x9A00, 0x009A
-    ; Data segment (privilege level 0)
     dw 0xFFFF, 0x0000, 0x9200, 0x0092
 gdt_end:
-
 gdt_ptr:
     dw gdt_end - gdt_start - 1
     dd gdt_start
-
-    ; Padding and boot signature
     times 510 - ($ - $$) db 0
     dw 0xaa55
