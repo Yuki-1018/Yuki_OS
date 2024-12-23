dd265e373e15065bf25dda61253206f3817a223f
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-12-23 11:03:39 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,11 +1,11 @@
-void kmain() {
-    char *vga_buffer = (char*)0xb8000;
-    const char *message = "boot successfully";
+void kernel_main() {
+    char* video_memory = (char*)0xb8000;
+    const char* message = "Welcome to Yuki OS Kernel";
     int i = 0;
-    while (message[i] != 0) {
-        vga_buffer[i * 2] = message[i];
-        vga_buffer[i * 2 + 1] = 0x0f;
+
+    while (message[i] != '\0') {
+        video_memory[i * 2] = message[i];     // 文字をセット
+        video_memory[i * 2 + 1] = 0x07;      // 白地に黒文字
         i++;
     }
-    while (1);
 }
