1c01b1edc0778471957f47ac4913df1cf5d6c7c8
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-12-23 10:36:56 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,16 +1,11 @@
-// kernel.c
 void kmain() {
     char *vga_buffer = (char*)0xb8000;
     const char *message = "boot successfully";
     int i = 0;
-
-    // Write the message to the video memory
     while (message[i] != 0) {
-        vga_buffer[i * 2] = message[i];     // Character code
-        vga_buffer[i * 2 + 1] = 0x0f; // Attribute byte (white on black)
+        vga_buffer[i * 2] = message[i];
+        vga_buffer[i * 2 + 1] = 0x0f;
         i++;
     }
-
-    // Infinite loop (to prevent the OS from halting)
     while (1);
 }
