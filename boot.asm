4205dd316572d1db1b56ce7bed8e4a9c7e31eaf6
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2025-02-01 15:48:52 +0900

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,23 +1,30 @@
+[BITS 16]        ; 16ビットモード
+[ORG 0x7C00]    ; BIOSがブートローダをロードするアドレス
 
-[org 0x7c00]  ; ブートローダー開始アドレス
-mov ax, 0x07C0
-add ax, 288
-mov ss, ax
-mov sp, 0x0000
+start:
+    xor ax, ax  ; AXレジスタを0にクリア
+    mov ds, ax  ; データセグメントを0に設定
+    mov es, ax  ; エクストラセグメントを0に設定
 
-; カーネルをメモリに読み込む
-mov bx, 0x0000
-mov es, bx
-mov ah, 0x02
-mov al, 0x01
-mov ch, 0x00
-mov cl, 0x02
-mov dh, 0x00
-int 0x13
+    ; カーネルをロードする
+    mov ah, 0x02    ; BIOS読み込み機能
+    mov al, 1       ; 読み込むセクタ数
+    mov ch, 0       ; シリンダ番号
+    mov cl, 2       ; セクタ番号 (ブートセクタの次から読み込む)
+    mov dh, 0       ; ヘッド番号
+    mov dl, 0       ; ドライブ番号 (フロッピーの場合)
+    mov bx, 0x1000  ; 読み込み先のメモリアドレス (ES:BX)
+    int 0x13        ; BIOSディスクサービス
 
-; カーネルへジャンプ
-jmp 0x0000:0x0800
+    jc disk_error   ; エラーが発生した場合
 
-; ブートローダー終了
-times 510-($-$$) db 0
-dw 0xAA55
+    ; カーネルにジャンプ
+    jmp 0x1000:0x0000
+
+disk_error:
+    ; エラー処理 (ここでは何もしない)
+    cli
+    hlt
+
+times 510-($-$$) db 0  ; ブートシグネチャまでのパディング
+dw 0xAA55              ; ブートシグネチャ
